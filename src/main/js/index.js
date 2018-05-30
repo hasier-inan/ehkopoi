4f2dfee81133237feb4f61374cd15fb0d0ad7f21
Hasier Iñan <hasiermetal@gmail.com>
2018-05-30 22:19:00 +0200

    Included a react maps component, updated with full screen listner.

--- src/main/js/index.js
+++ src/main/js/index.js
@@ -1,25 +1,59 @@
 import 'bootstrap';
 import 'jquery';
+import {compose, withProps} from "recompose";
+import ReactWindowResizeListener from 'window-resize-listener-react';
+
+import {
+  withScriptjs,
+  withGoogleMap,
+  GoogleMap,
+  Marker
+} from "react-google-maps";
 
 require("../sass/styles.scss");
 
 const React = require('react');
 const ReactDOM = require('react-dom');
 
+const mapsKey = 'updateme';
+const EhMap = compose(
+  withScriptjs,
+  withGoogleMap
+)((props) =>
+  <GoogleMap
+    defaultZoom={10}
+    options={{fullscreenControl: false, mapTypeControl: false, streetViewControl: false}}
+    defaultCenter={{lat: 43.2627106, lng: -2.3189}}>
+    {props.isMarkerShown && <Marker position={{lat: -34.397, lng: 150.644}} onClick={props.onMarkerClick}/>}
+  </GoogleMap>
+)
+
 class EhkoPoi extends React.Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {isMarkerShown: false, height: window.innerHeight};
+  }
 
-    render() {
-        return (
-            <div>
-                maps
-            </div>
-        )
-    }
+  markerSelected(marker) {
+    this.setState({isMarkerShown: false})
+    console.log(marker);
+  }
+
+  render() {
+    return (
+      <div>
+        <ReactWindowResizeListener onResize={(event) => this.setState({height: window.innerHeight})}/>
+        <EhMap
+          isMarkerShown={this.state.isMarkerShown}
+          onMarkerClick={this.markerSelected}
+          googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${mapsKey}&v=3.exp&libraries=geometry,drawing,places`}
+          loadingElement={<div style={{height: `100%`}}/>}
+          containerElement={<div style={{height: `${this.state.height}px`}}/>}
+          mapElement={<div style={{height: `100%`}}/>}
+        />
+      </div>
+    )
+  }
 }
 export default EhkoPoi;
